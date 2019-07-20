53ea8d509b674333564e24c9267c1aa9adbddffa
Ben Frey <spartanhooah@gmail.com>
2019-07-20 13:12:56 -0500

    make VetController return a collection of Vets to the Vet template, display that collection

--- pet-clinic-web/src/main/java/net/frey/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/net/frey/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,23 @@
 package net.frey.sfgpetclinic.controller;
 
+import net.frey.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
