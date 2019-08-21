bae5bc0ab96eba098d42be256764415524e4558e
Ben Frey <spartanhooah@gmail.com>
2019-08-20 20:53:33 -0500

    enhance owner entity

--- pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Owner.java
@@ -6,5 +6,8 @@ import java.util.Set;
 
 @Data
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 }
