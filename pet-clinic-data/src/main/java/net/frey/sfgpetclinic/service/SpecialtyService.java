3799e5b07adf36e0cf54a3c2efea90d6c19ca0b4
Ben Frey <spartanhooah@gmail.com>
2019-08-23 20:10:39 -0500

    create Specialty Service

---
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package net.frey.sfgpetclinic.service;
+
+import net.frey.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package net.frey.sfgpetclinic.service.map;
+
+import net.frey.sfgpetclinic.model.Specialty;
+import net.frey.sfgpetclinic.service.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        deleteById(id);
+    }
+}
