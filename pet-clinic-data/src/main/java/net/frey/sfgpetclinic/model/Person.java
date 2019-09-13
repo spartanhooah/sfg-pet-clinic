f3a3113897eebe5cdca7765845fe1f0d445960e4
spartanhooah <spartanhooah@gmail.com>
2019-09-13 17:06:39 +0000

    converted Owner (and mapped types) to JPA entity

--- pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Person.java
@@ -2,8 +2,15 @@ package net.frey.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 @Data
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 }
--- pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Pet.java
@@ -2,12 +2,30 @@ package net.frey.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 }
