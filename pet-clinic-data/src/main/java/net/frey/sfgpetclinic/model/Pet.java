a83f30f2d3d8c99de888f5a815891afac27ad7c4
spartanhooah <spartanhooah@gmail.com>
2019-09-13 17:23:15 +0000

    convert Visit to entity

--- pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/model/Pet.java
@@ -2,14 +2,18 @@ package net.frey.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -28,4 +32,7 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 }
