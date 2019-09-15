eca6232fdc1565240ef4da7731efb04f0aa75ca6
Ben Frey <spartanhooah@gmail.com>
2019-09-14 19:58:57 -0500

    add rest of JPA services

---
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/jpa/PetTypeJpaService.java
@@ -0,0 +1,48 @@
+package net.frey.sfgpetclinic.service.jpa;
+
+import net.frey.sfgpetclinic.model.PetType;
+import net.frey.sfgpetclinic.repository.PetTypeRepository;
+import net.frey.sfgpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetTypeJpaService implements PetTypeService {
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
