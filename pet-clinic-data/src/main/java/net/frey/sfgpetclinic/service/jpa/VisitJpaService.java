4fb471fe57f3e08c1ea1d18919d5e0ea8068d8fe
Ben Frey <spartanhooah@gmail.com>
2019-09-15 20:55:30 -0500

    add VisitJpaService

---
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/jpa/VisitJpaService.java
@@ -0,0 +1,48 @@
+package net.frey.sfgpetclinic.service.jpa;
+
+import net.frey.sfgpetclinic.model.Visit;
+import net.frey.sfgpetclinic.repository.VisitRepository;
+import net.frey.sfgpetclinic.service.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VisitJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
