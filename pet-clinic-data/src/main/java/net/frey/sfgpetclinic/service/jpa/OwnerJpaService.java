5d40573d3b8327df1821507f6f8937dd8bdd1791
spartanhooah <spartanhooah@gmail.com>
2019-09-16 14:32:19 +0000

    remove unnecessary fields

--- pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/jpa/OwnerJpaService.java
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/jpa/OwnerJpaService.java
@@ -15,13 +15,9 @@ import java.util.Set;
 @Profile("jpa")
 public class OwnerJpaService implements OwnerService {
     private final OwnerRepository ownerRepository;
-    private final PetRepository petRepository;
-    private final PetTypeRepository petTypeRepository;
 
-    public OwnerJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+    public OwnerJpaService(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
-        this.petRepository = petRepository;
-        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
