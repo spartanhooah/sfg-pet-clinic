c6a8f565a4c096051d2d20acb8076a68131752ea
Ben Frey <spartanhooah@gmail.com>
2019-09-15 20:51:12 -0500

    change map service naming convention, add VisitService and VisitMapService

---
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/VisitService.java
@@ -0,0 +1,7 @@
+package net.frey.sfgpetclinic.service;
+
+import net.frey.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/net/frey/sfgpetclinic/service/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package net.frey.sfgpetclinic.service.map;
+
+import net.frey.sfgpetclinic.model.Visit;
+import net.frey.sfgpetclinic.service.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
