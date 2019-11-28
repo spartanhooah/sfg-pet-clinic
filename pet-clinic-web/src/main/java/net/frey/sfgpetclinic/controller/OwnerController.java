18c9b5dd7dffad36226a44ca2e7a750ce5819b41
spartanhooah <spartanhooah@gmail.com>
2019-11-28 16:33:16 -0500

    partial implementation of view owner; model is broken and so is CSS

--- pet-clinic-web/src/main/java/net/frey/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/net/frey/sfgpetclinic/controller/OwnerController.java
@@ -3,7 +3,10 @@ package net.frey.sfgpetclinic.controller;
 import net.frey.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -24,5 +27,13 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    private ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+
+        return mav;
     }
 }
